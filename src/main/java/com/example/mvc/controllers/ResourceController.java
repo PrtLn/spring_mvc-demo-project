c8e34f6f16fee1a6236f9230516ecc6b3bc2b384
oleno <pirit.lena@gmail.com>
2017-03-28 15:43:34 +0300

    handling errors/exceptions with HandlerExceptionResolver interface

--- src/main/java/com/example/mvc/controllers/ResourceController.java
+++ src/main/java/com/example/mvc/controllers/ResourceController.java
@@ -34,7 +34,7 @@ public class ResourceController {
 		return "resource_add";
 	}
 	
-	@ExceptionHandler(Exception.class)
+	@ExceptionHandler(NullPointerException.class)
 	public String handleError(HttpServletRequest request) {
 		return "controller_error";
 	}
